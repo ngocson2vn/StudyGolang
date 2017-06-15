82188da5e5b17a2edd1e669e8d7a721416f07837
Son Nguyen <ngocson2vn@gmail.com>
2017-06-15 10:51:09 +0900

    Add test_split.go

---
+++ test_split.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+import "strings"
+import "os"
+
+func ExecShoryukenByTask(cluster string) {
+	label := fmt.Sprintf("%s_shoryuken", os.Getenv("ECS_TASK_NAME"))
+	command := os.Getenv("SHORYUKEN_COMMAND")
+
+	if command == "" {
+		fmt.Println("There is no shoryuken command in Jenkinsfile.")
+		return
+	}
+
+	for _, cmd := range strings.Split(command, ", ") {
+		ExecuteBatchTask(cmd, label, cluster, 1, 256)
+		fmt.Println()
+	}
+}
+
+func ExecuteBatchTask(command string, label string, cluster string, num int64, memory int64) {
+	fmt.Println(command)
+	fmt.Println(label, cluster, num, memory)
+}
+
+func main() {
+	os.Setenv("SHORYUKEN_COMMAND", "bundle exec shoryuken -r /home/ubuntu/walking_events/lib/default_sqs_worker.rb, bundle exec shoryuken -r /home/ubuntu/walking_events/lib/step_sqs_worker.rb")
+	os.Setenv("ECS_TASK_NAME", "WalkingEvents")
+	cluster := "ProductionTask"
+	ExecShoryukenByTask(cluster)
+}
